ea87891fd4f574d0b3a9ff991e7b86d20ba1aa92
Roger Andrews <theartintheraw@gmail.com>
2025-12-10 14:21:10 -0500

    Refactor script and link tags in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="dns-prefetch" href="https://survey-dashboard-omega.vercel.app" />
-        <link
-          rel="preconnect"
-          href="https://survey-dashboard-omega.vercel.app"
-          crossOrigin="anonymous"
-        />
+          <link rel="dns-prefetch" href="https://survey-dashboard-omega.vercel.app">
+          <link rel="preconnect" href="https://survey-dashboard-omega.vercel.app" crossorigin>
       </head>
       <body className="bg-white text-black">
         {children}
-        <Script
-          src="https://survey-dashboard-omega.vercel.app/tracker-optimized.min.js"
-          data-site-id="nextblog"
-          async
-        />
+
+        <script src="https://survey-dashboard-omega.vercel.app/tracker-optimized.min.js" data-site-id="test-site-1" async></script>
       </body>
     </html>
   )
